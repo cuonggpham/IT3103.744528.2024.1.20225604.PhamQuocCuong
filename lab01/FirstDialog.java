4016651f2c6e463b8587d921ed5300282eff7e03
cuonggpham <qcuonghls@gmail.com>
2024-09-25 17:14:32 +0700

    first commit

---
+++ lab01/FirstDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello Cuong! Hajimemashite");
+        System.exit(0);
+    }
+}
